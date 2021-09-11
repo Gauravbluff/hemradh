269d9608f72e639155f0826a246b7d45dfdbe487
gauravbluff <gaurav.kumarcsk@gmail.com>
2021-09-12 00:54:56 +0530

    hemaradh first commit

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    autoplay:true,
+    animateOut: 'fadeOut',
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.move').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (50 * lMouseX) / 50; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (5 * lMouseY) / 50;
+
+});
+
+moveBackground();
